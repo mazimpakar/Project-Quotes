076994b1b06a063650fcf8106eebddcccb171a34
mazimpakar <rosemazimpaka2@gmail.com>
2019-01-28 09:09:54 +0200

    add correction some error

--- app.component.ts
+++ app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-   quotes:string[];
-   constructor(){
 
-    this.quotes = ["But man is not made for defaut.A man can be destroyed not defeated","When you reach the end of your rope,tie a knot in it and hang on","There is nothing perment except change","You cannot shake hands with a clenched fist"]
-  }
 
- }
+ 
+ export class AppComponent {
+  goals = [
+      new Quote(1,'But man is not made for defaut.A man can be destroyed not defeated'),
+      new Quote(2,'There is nothing perment except change'),
+      new Quote(3, 'You cannot shake hands with a clenched fist'),
+      new Quote(4, 'You cannot shake hands with a clenched fist'),
+      
+}
+
  
 
