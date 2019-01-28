c3e018608643c007f8f631878b77173cc0fb8c24
mazimpakar <rosemazimpaka2@gmail.com>
2019-01-28 17:09:39 +0200

    add communication between child and parent

--- quote/quote.component.ts
+++ quote/quote.component.ts
@@ -15,14 +15,12 @@ export class QuoteComponent implements OnInit {
   
 
 ]
-toogleDetails(index){
-  this.quotes[index].showDescription = !this.quotes[index].showDescription;
-}
-        
+completeQuote(isComplete,index){
+  if (isComplete){
+      this.quotes.splice(index,1);
 
-  constructor() { }
-
-  ngOnInit() {
+  
 
   }
+ }
 }
