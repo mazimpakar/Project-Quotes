7232d18519a9370c680379b6999400dfba6a0a25
mazimpakar <rosemazimpaka2@gmail.com>
2019-01-28 18:57:06 +0200

    add transfomation of input to output

--- quote/quote.component.ts
+++ quote/quote.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {Quote} from '../quote'
 
 @Component({
@@ -7,20 +9,24 @@ import {Quote} from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
   quotes = [
-    new Quote(1,'When you reach the end of your hope,tie a knot in it hang on','Find an online  '),
-    new Quote(2,'there is nothing permanent excpt change','it is true'),
-    new Quote(3, 'life what happens when you are busy making other plans','check internet'),
+        new Quote(1,'When you reach the end of your hope,tie a knot in it hang on','Find an online', new Date(2018,12,31)),
+        new Quote(2,'there is nothing permanent excpt change','it is true', new Date (2018,11,15)),
+        new Quote(3, 'life what happens when you are busy making other plans','check internet' , new Date(2018,10,2)),
+      
+    
+    ]
+    completeGoal(isComplete,index){
+      if (isComplete){
+          this.quotes.splice(index,1);}}
+
+  @Input() goal:Quote;
+  @Output() isComplete= new EventEmitter<boolean>();
+
   
+  constructor() { }
 
-]
-completeQuote(isComplete,index){
-  if (isComplete){
-      this.quotes.splice(index,1);
-
-  
-
+  ngOnInit() {
   }
- }
+
 }
