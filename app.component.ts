3bff9d6e66f6922304f75ec4b05b28f80b9cd01e
mazimpakar <rosemazimpaka2@gmail.com>
2019-01-29 17:13:59 +0200

    add correct error

--- app.component.ts
+++ app.component.ts
@@ -1,18 +1,9 @@
-import { Component } from '@angular/core';
-import { Quote } from './quote'
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-
-  quotes = [
-    // new Quote(1,'1',new Date(2005,2,2)),
-    // new Quote(2,'there is nothing permanent excpt change'),
-    // new Quote(3, 'life what happens when you are busy making other plans'),
-
-
-  ]
-
+  title = "Quotes";
 }
